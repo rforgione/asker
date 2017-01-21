3c8a4a144b02ce0a22e1749e1facfd928ccd97b8
rforgione <rforgione@gmail.com>
2017-01-20 22:29:42 -0500

    fix structure

--- asker/dbtools/setpath.py
+++ asker/dbtools/setpath.py
@@ -2,11 +2,6 @@ import sys
 import os
 
 def setpath():
-    print os.path.dirname(
-            os.path.dirname(
-                os.path.dirname(os.path.realpath(__file__))
-            )
-        )
     sys.path.append(
         os.path.dirname(
             os.path.dirname(
--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'asker/data/app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'app/data/db_repository')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'asker/data/db_repository')
